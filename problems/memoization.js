952512a24ed36c0f8012f337007067dbb659e4bb
Maureen Durnin <maureenmdurnin@gmail.com>
2019-12-20 14:26:07 -0800

    add insertion sort selection sort bubble sort and tabulation projects

--- problems/memoization.js
+++ problems/memoization.js
@@ -10,4 +10,40 @@ function fastFib(num, memo={}) {
     return memo[num]
 }
 
-console.log(fastFib(5))
+// console.log(fastFib(5))
+
+
+// minChange([1, 2, 5], 11)         // => 3, because 5 + 5 + 1 = 11
+// minChange([1, 4, 5], 8))         // => 2, because 4 + 4 = 8
+// minChange([1, 5, 10, 25], 15)    // => 2, because 10 + 5 = 15
+// minChange([1, 5, 10, 25], 100)   // => 4, because 25 + 25 + 25 + 25 = 100
+
+// function minChange(coins, amount, memo = {}) {
+//     if (amount === 0) return 0;
+//     // if (coins.includes(amount)) return 1;
+
+//     let numCoins = [];
+//     coins.forEach((coin) => {
+//         numCoins.push(minChange(coins, amount - count) + 1);
+//     });
+
+
+// }
+
+function minChange(coins, amount, memo = {}) {
+  if (amount === 0) return 0;
+  if (coins.includes(amount)) return 1;
+
+  let coin_count = 0;
+
+  for (let i = coins.length - 1; i >= 0; i--) {
+    if (amount > coins[i]) {
+      amount -= coins[i];
+      coin_count = coin_count + minChange(coins, amount, memo);
+      console.log(coin_count)
+      return coin_count;
+    }
+  }
+}
+
+console.log(minChange([1, 5, 10, 25], 100))
