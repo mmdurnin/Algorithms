082f0da9854279d48371dc511738a41e6f6c52bc
Maureen Durnin <maureenmdurnin@gmail.com>
2020-01-12 22:22:27 -0800

    add subsets

--- problems/misc_problems.rb
+++ problems/misc_problems.rb
@@ -173,7 +173,7 @@ def advanceCoords(direction, coords)
 end
 
 puts "generating spiral"
-print generate_spiral(1)
+# print generate_spiral(1)
 
 
 # Given a non-empty array of digits representing a non-negative integer, plus one to the integer.
@@ -201,3 +201,36 @@ end
 #         @next = nil
 #     end
 # end  
+
+require 'set'
+def subsets(nums)
+    result = sub_sets(nums)
+    result.push([])
+end
+
+def sub_sets(nums, storage_set=Set.new)
+    return if nums.length <= 0
+    return if storage_set.include?(nums)
+
+    storage_set.add(nums)
+    
+    nums.each_with_index do |num, idx|
+        storage_set.add([num])
+        temp = nums[0...idx] + nums[idx+1..-1]
+        # reversed = temp.reverse
+        storage_set.add([num])
+        # storage_set.add(temp)
+        # storage_set.add(reversed)
+        subs = sub_sets(temp, storage_set)
+        if !!subs 
+            subs.each do |sub| 
+                storage_set.add(sub) if (!storage_set.include?(sub)) && !!sub
+            end
+        end
+        # storage_set.add(subsets(reversed, storage_set))
+    end
+    return storage_set.to_a
+end
+
+
+# p subsets([1,2,3,4,5,6,7,8,10,0])
