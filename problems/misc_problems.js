6847816e6cbd3433ef55facbfe4248b2dbd726e1
Maureen Durnin <maureenmdurnin@gmail.com>
2020-01-15 17:44:27 -0800

    add prototypal inheritance info

--- problems/misc_problems.js
+++ problems/misc_problems.js
@@ -108,6 +108,28 @@ function timeConversion(s) {
     return splitTime.join(":")
 }
 
-console.log(timeConversion("07: 05: 45PM"))
-console.log(timeConversion("12: 05: 45PM"))
-console.log(timeConversion("12: 05: 45AM"))
+// console.log(timeConversion("07: 05: 45PM"))
+// console.log(timeConversion("12: 05: 45PM"))
+// console.log(timeConversion("12: 05: 45AM"))
+
+
+
+
+/*
+Write a function that takes an array and a target sum. If two numbers added together = the target sum, return the 
+two numbers in an array, else return an empty array.
+
+*/
+function twoNumberSum(array, targetSum) {
+    let visited = {};
+    for (let i = 0; i < array.length; i++) {
+        let num = array[i];
+        let remainder = targetSum - num;
+
+        if (!!visited[remainder]) return [remainder, num]
+        !!visited[num] ? visited[num].push(i) : visited[num] = i;
+    }
+    return []
+}
+
+console.log(twoNumberSum([1, 2, 3, 4, 5, 6, 7, 8, 9], 17))
