f73dd90ca1722224ef23a97bd8ef990cf4096b5b
Maureen Durnin <maureenmdurnin@gmail.com>
2020-01-17 17:28:38 -0800

    complete combinations decoding problem from mock interview demo

--- problems/misc_problems.rb
+++ problems/misc_problems.rb
@@ -332,4 +332,39 @@ def find_dups(arr)
     return mults.to_a
 end
 
-p find_dups([1, 2, 2, 3, 4, 5, 5, 5, 6, 7, 8, 8, 9, 10])
+# p find_dups([1, 2, 2, 3, 4, 5, 5, 5, 6, 7, 8, 8, 9, 10])
+
+# Write a method that will take a number and retturn a count of the number of "secret codes" it will return
+# The secret code is such that every letter in the alphabet maps to a letter
+# ex: 
+# 12258:
+# abbeh
+# aveh
+# abyh
+# lbeh
+# lyh
+
+def decode(code)
+    str = code.to_s
+    return str.length if str.length <= 1
+
+    count = 1
+    i = 0
+    while i < str.length - 1
+        number = str[i].to_i
+        next_num = str[i+1].to_i
+        count += 1 if number == 1
+        count += 1 if number == 2 && next_num <= 6
+
+        i += 1
+    end
+
+    return getFib(count)
+end
+
+def getFib(count)
+    return count if count <= 2
+    return getFib(count - 2) + getFib(count - 1)
+end
+
+p decode(12258)
