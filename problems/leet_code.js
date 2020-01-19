cee51e58c456631e9f24c357edf2111b84aa5a01
Maureen Durnin <maureenmdurnin@gmail.com>
2020-01-18 17:53:10 -0800

    add to leetcode

--- problems/leet_code.js
+++ problems/leet_code.js
@@ -53,22 +53,34 @@ isValid = function(s) {
     return true;
 }
 
-console.log(isValid("(){}[]"));
+// console.log(isValid("(){}[]"));
 
 
 // Given an array nums of n integers where n > 1,  return an array output such that output[i] is equal to 
 // the product of all the elements of nums except nums[i].
 
 var productExceptSelf = function (nums) {
-    let sum = 0;
+    let left = 1
     for (i = 0; i < nums.length; i++) {
-        let num = nums[i];
-        sum *= num;
+        let arr = []
+        arr.push(left)
+        left *= nums[i]
+        arr.push(nums[i])
+        arr.push(left)
+        nums[i] = arr
     }
-    for (i = 0; i < nums.length; i++) {
-        let num = nums[i];
-        let diff = sum - num;
-        nums[i] = diff;
+    let right = 1
+    let middle = 1
+    for (i = nums.length - 1; i >= 0; i--) {
+       let arr = nums[i];
+       left = arr[0]
+       middle = left * right
+       right *= arr[1]
+       nums[i] = middle;
     }
     return nums;
 };
+
+console.log(productExceptSelf([1, 2, 3, 4])) // [24, 12, 8, 6]
+console.log(productExceptSelf([5, 2, 3, 4])) // [24, 60, 40, 30]
+console.log(productExceptSelf([0, 2, 3, 4])) // [24, 0, 0, 0]
