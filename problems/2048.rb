2065d68b38a1c75dd1582b54e7499b14f74ebfdb
Maureen Durnin <maureenmdurnin@gmail.com>
2020-01-22 10:56:33 -0800

    add more system design prompts

---
+++ problems/2048.rb
@@ -0,0 +1,6 @@
+# Design 2048
+
+# Game: number of open tiles, start game, round, game over?
+# Player: score, move (down/ up)
+# Board: tiles, place new tile randomly, position, shiftup, shiftdown, shiftleft, shiftright
+# Tiles: new tile(generate random #), merge
