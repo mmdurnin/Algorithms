58962c631e331ae13dafe6b125256ad06ad9919e
Maureen Durnin <maureenmdurnin@gmail.com>
2020-01-27 22:20:19 -0800

    solve pramp interview q diff between two strings

--- problems/pramp.js
+++ problems/pramp.js
@@ -1,161 +1,54 @@
-//   // ABCDEFG
-//   // ABDFFGH
-//   // output ->> A B -C D -E F +F G +H
-
-// // this doesnt work:
-
-// function diffBetweenTwoStrings(source, target) {
-// 	/**
-// 	@param source: string
-// 	@param target: string
-// 	@return: string[]
-// 	*/
-
-//     // ABCDEFG
-//     // ABDFFGH
-
-//     let result = [];
-//     const sourceStore = {};
-//     const targetStore = {};
-//     const longerWord = source.length > target.length ? source : target
-
-//     for (let i = 0; i < longerWord.length; i++) {
-
-//         if (!!source[i]) {
-//             let sourceLetter = source[i]
-//             sourceStore[sourceLetter] = sourceStore[sourceLetter] ? sourceStore[sourceLetter] + 1 : 1
-//         };
-
-//         if (!!target[i]) {
-//             let targetLetter = target[i];
-//             targetStore[targetLetter] = targetStore[targetLetter] ? targetStore[targetLetter] + 1 : 1
-//         }
-//     }
-
-//     Object.keys(sourceStore).forEach((key) => {
-//         if (!!targetStore[key]) {
-//             // find the minimum # of times they both occur and push that in without a sign
-//             let min = targetStore[key] > sourceStore[key] ? sourceStore[key] : targetStore[key]
-//             result = result.concat(pushNumTimes(min, key, ""));
-
-//             // find the # of times it occuurs in one string and not the other
-//             // if occurs in target, push with +
-//             // if occurs in souce, push with -
-//             let diff = targetStore[key] - sourceStore[key];
-//             let sign = diff > 0 ? "+" : "-";
-
-//             if (diff !== 0) result = result.concat(pushNumTimes(diff, key, sign));
-//         } else {
-//             console.log("it didn't occur in target")
-//             // if it only occurs in souce and not target, push in with -
-//             result = result.concat(pushNumTimes(sourceStore[key] * -1, key, "-"))
-//         }
-//     })
-
-//     Object.keys(targetStore).forEach((key) => {
-//         if (!sourceStore[key]) {
-//             result = result.concat(pushNumTimes(targetStore[key], key, "+"))
-//         }
-//     })
-
-//     return result;
-// }
-
-// function pushNumTimes(diff, letter, sign) {
-//     let arr = [];
-//     let numTimes = Math.abs(diff)
-//     for (i = 0; i < diff; i++) {
-//         if (sign === "-") {
-//             arr.push(`-` + letter);
-//         } else if (sign === "+") {
-//             arr.push(`+` + letter);
-//         } else {
-//             arr.push(letter);
-//         }
-//     }
-
-//     return arr
-// }
-
-
-
-
 function diffBetweenTwoStrings(source, target) {
-    // A B /C/ D /E/ F  _ G _
-    // A B  _  D  _ F /F/ G H
-    //>> A B -C D -E F +F G +H
 
-    //  "CBBC"
-    //  "CABAABBC"
-    //>> ["C","-A","B","-A","-A","-A","B","-B","C"]
-
-    // "AABACC"  A  ABACC B  ACC  C  C  C
-    // "BABCAC"  A  BCAC  B  CAC  C  AC C
-    //>> ["-A","-A","B","A","+B","C","+A","C"]
-
-
-
-    // ideas:
-    // -sort?
-    // -double loop?
-    // -hash storage?
-    arr = [];
-    if (source.length === 1 && target.length === 1) {
-        return source[0] === target[0] ? [source[0]] : [(neg(source[0])), (pos(target[0]))];
-
-    } else if (source.length === 1) {
-        console.log("source")
-        console.log(source)
-        console.log("target")
-        console.log(target)
-        let compare = true;
-        for (i = 0; i < target.length; i++) {
-            if (target[i] === source[0] && compare === true) {
-                arr.push(target[i]);
-                compare = false;
-            } else {
-                arr.push(pos(target[i]));
-            }
-        }
-        console.log("arr")
-        console.log(arr)
-        return arr;
-
-    } else if (target.length === 1) {
-        let compare = true;
-        for (i = 0; i < source.length; i++) {
-            if (source[i] === target[0] && compare === true) {
-                arr.push(source[i]);
-                compare = false;
-            } else {
-                arr.push(neg(source[i]));
-            }
-        }
-        return arr;
+    let store = [];
+    
+    while (source.length && target.length && source[0] === target[0]) {
+        store.push(source[0]);
+        source = source.slice(1);
+        target = target.slice(1);
     }
 
 
-    if (source[0] === target[0]) {
-        return [source[0]].concat(diffBetweenTwoStrings(source.slice(1), target.slice(1)));
-    } else {
-        let poss1 = diffBetweenTwoStrings(source.slice(1), target);
-        let poss2 = diffBetweenTwoStrings(source, target.slice(1));
 
-        if (poss2.length > poss1.length) {
-            return [pos(target[0])].concat(poss1);
-        } else {
-            return [neg(source[0])].concat(poss2);
-        }
-    }
+    let optionA = explore(store, neg(source[0]), source.slice(1), target);
+    let optionB = explore(store, pos(target[0]), source, target.slice(1));
 
+    return optionA.length < optionB.length ? optionA : optionB;
 }
 
+function explore(store, edit, source, target) {
+    store = store.concat(edit)
+
+    
+    if (!source.length && !target.length) return store;
+    if (!source.length) return append(store, "pos", target);
+    if (!target.length) return append(store, "neg", source);
+
+    while (!!source.length && !!target.length && source[0] === target[0]) {
+        store.push(source[0]);
+        source = source.slice(1);
+        target = target.slice(1);
+    }
+
+    let optionA = explore(store, neg(source[0]), source.slice(1), target);
+    let optionB = explore(store, pos(target[0]), source, target.slice(1));
+    return optionA.length > optionB.length ? optionB : optionA;
+}
+
+function append(store, sign, tail) {
+    for (i = 0; i < tail.length; i++) {
+        store = store.concat(sign === "pos" ? pos(tail[i]) : neg(tail[i]))
+    }
+    return store;
+}
 
 function neg(key) {
+    if (!key) return [];
     return "-" + key
 }
 
 function pos(key) {
+    if (!key) return [];
     return "+" + key
 }
 
@@ -163,3 +56,10 @@ console.log("ABCDEFG", "ABDFFGH")
 console.log(diffBetweenTwoStrings("ABCDEFG", "ABDFFGH"))
 console.log("CABAAABBC", "CBBC")
 console.log(diffBetweenTwoStrings("CABAAABBC", "CBBC"))
+
+// console.log(append(["a", "b", "c"], "pos", ["B", "E", "M"]))
+
+
+
+// Given an array of unique characters arr and a string str, 
+// Implement a function getShortestUniqueSubstring that finds the smallest substring of str containing all the characters in arr. Return "" (empty string) if such a substring doesn’t exist.
