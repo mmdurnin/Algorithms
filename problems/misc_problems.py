c9bd4b01d512b0b93540aaf88b4e3070209b3997
Maureen Durnin <maureenmdurnin@gmail.com>
2020-02-03 12:08:40 -0800

    add python notes

---
+++ problems/misc_problems.py
@@ -0,0 +1,31 @@
+print("hello world")
+x = 4
+if x == 4:
+    print ("hello world")
+print("now I'll continue")
+myfloat = 7.0
+print(myfloat)
+myfloat = float(7)
+print(myfloat)
+
+mystring = 6.2
+if mystring == 6.2:
+    print("String: %f" % mystring)
+
+# %d == I'm going to put an integer here
+# %s == I'm going to put a string here
+# %f == I'm going to put a float here
+
+mylist = []
+mylist.append(2)
+print(mylist)
+print(mylist[0])
+print(mylist[-1])
+
+# also concat arrays by "+" and "*"
+arr1 = [1, 2, 3, 4, 5]
+arr2 = [6, 7, 8, 9, 10]
+print(arr1 + arr2)
+print(arr1 * 3)
+
+# length = "len"
