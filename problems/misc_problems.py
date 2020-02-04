9c3a1029848d559e173781fdb22d2be074549943
Maureen Durnin <maureenmdurnin@gmail.com>
2020-02-04 14:30:53 -0800

    add number palindrome problem to python

--- problems/misc_problems.py
+++ problems/misc_problems.py
@@ -102,7 +102,19 @@ def romanToInt(s):
 
 # print(romanToInt("III"))
 
-num = 700
-base = len(`num`) 
-print(`num`[0])
-print(base)
+# num = 700
+# base = len(`num`) 
+# print(`num`[0])
+# print(base)
+
+
+
+# Determine whether an integer is a palindrome. An integer is a palindrome 
+# when it reads the same backward as forward.
+def palindrome_num(num):
+    if num <  0: return false
+    str = `num`
+    for idx, char in enumerate(str):
+        if char != str[len(str) - idx - 1]: return false
+    return true
+    
