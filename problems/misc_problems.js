573cecf19503e73a88f165d901affe4882f96357
Maureen Durnin <maureenmdurnin@gmail.com>
2020-02-03 18:44:49 -0800

    add python notes, solve roman numeral conversion in python

--- problems/misc_problems.js
+++ problems/misc_problems.js
@@ -132,4 +132,38 @@ function twoNumberSum(array, targetSum) {
     return []
 }
 
-console.log(twoNumberSum([1, 2, 3, 4, 5, 6, 7, 8, 9], 17))
+// console.log(twoNumberSum([1, 2, 3, 4, 5, 6, 7, 8, 9], 17))
+
+
+
+// roman numerals exercise
+var romanToInt = function (s) {
+    // store conversion key (e.g., {"I": 1, "V": 5 ...})
+    // store a state that is either "add" or "subtract"
+    // read from right to left
+    // add 1, 5, 10, 50, 100, 500, 1000 depending on key if state is "add"
+    // subtract if state = "subtract"
+    // change state to "add"
+    // set conditionals for changing state to subtract (if current === "X" and next === "I", change state to "subtract")
+
+    let result = 0;
+    let key = { I: 1, V: 5, X: 10, L: 50, C: 100, D: 500, M: 1000 };
+    let marker = "add"
+    for (let i = s.length - 1; i >= 0; i--) {
+        let char = s[i];
+        let value = key[char];
+        if (marker === "add") result += value;
+        if (marker === "subtract") result -= value;
+
+        if (i > 0) {
+            marker = "add";
+            let compareChar = s[i - 1];
+            if ((char === "V" || char === "X") && compareChar === "I") marker = "subtract";
+            if ((char === "L" || char === "C") && compareChar === "X") marker = "subtract";
+            if ((char === "D" || char === "M") && compareChar === "C") marker = "subtract";
+        }
+    }
+    return result;
+};
+
+console.log(romanToInt("III"))
--- problems/misc_problems.py
+++ problems/misc_problems.py
@@ -29,3 +29,80 @@ print(arr1 + arr2)
 print(arr1 * 3)
 
 # length = "len"
+
+
+astring = "Hello world!"
+print(astring[3:7])
+print(astring[3:7:2]) # prints range 3 - 7, skipping by 2 each time, it would print "l" and " "
+
+# Prints out 0,1,2,3,4 and then it prints "count value reached 5"
+
+count=0
+while(count<5):
+    print(count)
+    count +=1
+else:
+    print("count value reached %d" %(count))
+# prints the end result after condition is no longer met, then ends
+
+# Prints out 1,2,3,4
+for i in range(1, 10):
+    if(i%5==0):
+        break
+    print(i)
+else:
+    print("this is not printed because for loop is terminated because of break but not due to fail in condition")
+# there was a break so it wasn't printed. If there weren't a break, it wouldn't print
+
+
+numbers = [
+    951, 402, 984, 651, 360, 69, 408, 319, 601, 485, 980, 507, 725, 547, 544,
+    615, 83, 165, 141, 501, 263, 617, 865, 575, 219, 390, 984, 592, 236, 105, 942, 941,
+    386, 462, 47, 418, 907, 344, 236, 375, 823, 566, 597, 978, 328, 615, 953, 345,
+    399, 162, 758, 219, 918, 237, 412, 566, 826, 248, 866, 950, 626, 949, 687, 217,
+    815, 67, 104, 58, 512, 24, 892, 894, 767, 553, 81, 379, 843, 831, 445, 742, 717,
+    958, 609, 842, 451, 688, 753, 854, 685, 93, 857, 440, 380, 126, 721, 328, 753, 470,
+    743, 527
+]
+
+for number in numbers:
+    print(number)
+    if number == 402:
+        break
+
+strings = ("test1", "test2")
+print(strings)
+
+
+str = "maureen"
+reve = str[::-1]
+print(reve)
+
+def romanToInt(s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        key = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        result = 0
+        reverstr = s[::-1]
+        state = "add"
+        for i, ch in enumerate(reverstr):
+            value = key[ch]
+            if state == "add": result += value 
+            if state == "subtract": result -= value 
+            
+            if i < len(s) - 1:
+                state = "add"
+                compare = s[i + 1]
+                if (compare == "V" or compare == "X") and ch == "I": state = "subtract" 
+                if (compare == "L" or compare == "C") and ch == "X": state = "subtract" 
+                if (compare == "D" or compare == "M") and ch == "C": state = "subtract" 
+        return result
+
+# print(romanToInt("III"))
+
+num = 700
+base = len(`num`) 
+print(`num`[0])
+print(base)
