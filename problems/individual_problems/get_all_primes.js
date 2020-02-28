4a6f99bec4891fa6a830fa3687053cafa7a694d8
Maureen Durnin <maureenmdurnin@gmail.com>
2020-02-28 11:38:39 -0800

    complete get all primes

--- problems/individual_problems/get_all_primes.js
+++ problems/individual_problems/get_all_primes.js
@@ -3,3 +3,35 @@
 // a factors appears multiple time in the decomposition it should appear the
 // same number of times in the array.
 //     exemple: getAllPrimeFactors(100) returns[2, 2, 5, 5] in this order.
+
+function getAllPrimes(number) {
+  let primes = [];
+  for (let i = 2; i <= number;) {
+    if (number % i === 0) {
+      if (isPrime(i)) {
+        primes.push(i)
+      } else {
+        primes = primes.concat(getAllPrimes(i))
+      }
+      number = number / i;
+      i = 2;
+    } else {
+      i++;
+    }
+  }
+  return primes;
+};
+
+function isPrime(num) {
+  if (num < 2) return false;
+
+  let counter = 2;
+  while (counter < num) {
+    if (num % counter === 0) return false;
+    counter++;
+  };
+  
+  return true;
+};
+
+console.log(getAllPrimes(15))
