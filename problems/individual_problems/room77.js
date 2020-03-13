d7a84f2f0a2ae03aba4def5a0f8d14f067cf8715
Maureen Durnin <maureenmdurnin@gmail.com>
2020-03-12 17:08:40 -0700

    add room77 questions

---
+++ problems/individual_problems/room77.js
@@ -0,0 +1,22 @@
+/*
+A single-story house is built on an NxM grid where all walls are along grid lines. For example, the figure here shows a possible floor plan which has 5 rooms. A "room" is a connected area enclosed by walls.
+
+Your goal is to remove one wall (of length 1) from the floor plan to make as large a room as possible.
+How do you write a program to determine which wall to remove?
+*/
+
+/*
+Lufthansa flight 458 flies from Munich, Germany to San Francisco, USA. It leaves Munich at around 4pm local time, and arrives in San Francisco at about 7pm local time on the same day.
+
+Normally, when you fly from east to west, you "follow the sun" and the sun sets very slowly over the 12-hour flight. However, when I took the flight last December, it started out in daylight, then it got dark quickly, then I saw daylight again, and then it got dark again before arrival.
+
+How do you explain this phenomenon?
+
+In the northern hemisphere it gets darker much earlier in winter because the earth orbits around the sun on a tilt and
+in the winter, the southern hemisphere tilts towards the sun. The luftansa flight from Munich to SFO follows a parabala 
+moving northward in the beginning and southward in the end. While you were moving north, the sun was making the journey to san francisco
+at a faster pace than you. In the north, a much smaller area is covered by the sun during winter. Because of these two 
+factors, you fell out of the sun zone. During the second part of your trip, you started moving toward san francisco, slowly "catching up" 
+with the sun and then south again, where the sun casts a larger net, thereby reaching sun zone. As you slow down, the sun 
+keeps moving westward and you lose it again. Those crazy parabolas.
+*/
