e1d3998130cd4be8fe69874dd30f5d435aff7ae2
Maureen Durnin <maureenmdurnin@gmail.com>
2020-03-14 18:02:43 -0400

    update string to int

--- problems/individual_problems/string_to_int.js
+++ problems/individual_problems/string_to_int.js
@@ -11,36 +11,47 @@ If no valid conversion could be performed, a zero value is returned.
 */
 
 function atoi(str) {
-  // if the string contains +- or -+ togther, return 0
-  if (str.includes('+-') || str.includes('-+'))
+  if (str.includes('+-') || str.includes('-+') || str === "")
     return 0;
-    for (i in str) {
-      let char = str[i];
 
-      if (char !== " ")
-        if (char === "-" || char === "+" || Number(char)) return numberize(str.slice(i));
-        if (char === NaN) return 0;
+  for (i in str) {
+    let char = str[i];
+
+    if (char !== " ")
+      if (char === "-" || char === "+" || Number(char) || char === '0') return numberize(str.slice(i));
+      if (String(Number(char)) === 'NaN') return 0;
   }
+  return 0
 }
 
 function numberize(str) {
   let i = 0, s = "";
   
   if (str[i] === "+" || str[i] === "-")
-  s = s.concat(str[i])
-  i++
+    s = s.concat(str[i])
+    i++
   
-  while (Number(str[i]) && i < str.length) {
+  console.log(str)
+  while ((Number(str[i]) || str[i] === '0') && i < str.length) {
     let char = str[i];
     s = s.concat(char)
     i++
   } 
+
   let result = Number(s);
+  console.log(result)
+  if (!result) return 0;
+
   if (Math.abs(result) > 2**31 - 1) {
     result = result < 0 ? Math.pow(-2, 31) : Math.pow(2, 31) - 1;
   };
   return result;
 }
 
-console.log(atoi("   -42"))
-console.log(atoi("-91283472332"))
+// console.log(atoi("   -42"))
+// console.log(atoi("-91283472332"))
+// console.log(atoi("42"))
+// console.log(atoi("words and 987"))
+// console.log(atoi(""))
+// console.log(atoi("    0000000000000   "))
+console.log(atoi("  0000000000012345678"))
