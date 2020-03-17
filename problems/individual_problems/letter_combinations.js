fbccbc2aa4abf86eb1c0c5bf8eb20ffc57464c21
Maureen Durnin <maureenmdurnin@gmail.com>
2020-03-17 10:42:15 -0700

    solve letter combinations

--- problems/individual_problems/letter_combinations.js
+++ problems/individual_problems/letter_combinations.js
@@ -1,8 +1,6 @@
-var letterCombinations = function (digits, memo={}, str="", flag=true) {
-  console.log("*")
-  // if (digits in memo) return memo[digits];
+var letterCombinations = function (digits, str="") {
   if (!digits.length) return str;
-  let result = new Set();
+  let result = [];
 
   let key = { 2: ["a", "b", "c"], 
               3: ["d", "e", "f"], 
@@ -13,27 +11,20 @@ var letterCombinations = function (digits, memo={}, str="", flag=true) {
               8: ["t", "u", "v"], 
               9: ["w", "x", "y", "z"] };
 
-  for (let i = 0; i < digits.length; i++) {
-    let digit = digits[i];
-    let newDigits = digits.slice(0, i).concat(digits.slice(i+1));
-    let arr = key[digit];
-    memo[newDigits] = [];
+  let digit = digits[0];
+  let newDigits = digits.slice(1);
+  let arr = key[digit];
+  let store = [];
 
-    for (let j = 0; j < arr.length; j++) {
-      let letter = arr[j];
-      let tempStr = str.concat(letter);
-      console.log(!!flag)
-      let newCombos = letterCombinations(newDigits, memo, tempStr, false)
-      memo[newDigits] = memo[newDigits].concat(newCombos)
-    }
-    console.log("inside")
-    console.log(memo[newDigits])
-    console.log(!!flag)
-    result.add(memo[newDigits])
+  for (let j = 0; j < arr.length; j++) {
+    let letter = arr[j];
+    let tempStr = str.concat(letter);
+    let newCombos = letterCombinations(newDigits, tempStr)
+    store = store.concat(newCombos)
   }
-  console.log("result")
-  console.log(result)
-  return Array.from(result)
+  result = result.concat(store)
+  return result
 };
 
-console.log(letterCombinations("234"))
+console.log(letterCombinations("2356"))
+console.log(letterCombinations(""))
