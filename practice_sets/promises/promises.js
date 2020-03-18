a061d195b96cc4785cee28f05ddc6acb86fd810f
Maureen Durnin <maureenmdurnin@gmail.com>
2020-03-17 23:27:23 -0700

    add more promise notes

--- practice_sets/promises/promises.js
+++ practice_sets/promises/promises.js
@@ -72,3 +72,62 @@ const todoList = fetch(`https://jsonplaceholder.typicode.com/todos`).then(res =>
 
 Promise.all([diyPromise, users, todoList])
   .then((values) => {console.log(values)})
+
+
+async function spaceConsoleLogs() {
+  let i = 1;
+  while (i < 5) {
+    i = await increment(i)
+    console.log(i)
+  }
+}
+
+function increment(i) {
+  return new Promise((resolve, reject) => {
+    return setTimeout(() => {
+      i++
+      resolve(i)
+    }, 2000)
+  })
+}
+
+// spaceConsoleLogs()
+
+// First promise returns an array
+const getUsers = () => {
+  return new Promise((resolve, reject) => {
+    return setTimeout(() => resolve([{ id: 'jon' }, { id: 'andrey' }, { id: 'tania' }]), 600)
+  })
+}
+
+// Second promise relies on the resulting array of first promise
+const getIdFromUser = users => {
+  return new Promise((resolve, reject) => {
+    return setTimeout(() => resolve(users.id), 500)
+  })
+}
+
+// Third promise relies on the result of the second promise
+const capitalizeIds = id => {
+  return new Promise((resolve, reject) => {
+    return setTimeout(() => resolve(id.toUpperCase()), 200)
+  })
+}
+
+const runAsyncFunctions = async () => {
+  const users = await getUsers()
+
+  Promise.all(
+    users.map(async user => {
+      const userId = await getIdFromUser(user)
+      console.log(userId)
+
+      const capitalizedId = await capitalizeIds(userId)
+      console.log(capitalizedId)
+    })
+  )
+
+  console.log(users)
+}
+
+runAsyncFunctions()
