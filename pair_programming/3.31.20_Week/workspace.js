1c23e85dbe32005f4a16a922ef982e8b4f3c8576
Maureen Durnin <maureenmdurnin@gmail.com>
2020-04-01 12:06:15 -0700

    add camping trip to this weeks pair programming problems

--- pair_programming/3.31.20_Week/workspace.js
+++ pair_programming/3.31.20_Week/workspace.js
@@ -26,3 +26,25 @@ output: 5
 explanation: [[1, 3], [2, 4], [3, 5], [4, 6], [5, 7]]
 
 */
+
+
+/*
+Write a function/ program that will calculate expenses for a given group of people.
+Here is the prompt with an example:
+
+Sarah rents a car for the trip - she pays $400 for the car, which is used by Alice, John, Bob and herself.
+Later in the trip, John went out and bought groceries for $100, which he shared with Alice and Bob.
+Now, the trip is over and everyone wants to get paid back what they are owed
+- print out the list of transactions that would settle everyone's debts.
+
+This is a flexible exercise. The solution that I came up with has the following inputs and outputs:
+
+let campingTrip = new ExpenseRecord();
+let [sarah, john, bob, alice] = campingTrip.addMembers("Sarah", "John", "Bob", "Alice");
+campingTrip.addExpense(sarah, [alice, john, bob], 400)
+campingTrip.addExpense(john, [alice, bob], 100)
+campingTrip.printExpenses() // -> Sarah is owed $300
+                               -> Alice owes $133
+                               -> John owes $33
+                               -> Bob owes $133
+*/
