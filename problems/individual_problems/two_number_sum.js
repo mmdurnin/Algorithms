3a46c4415b4c335c3724e7a78c12d057e3ac3daa
Maureen Durnin <maureenmdurnin@gmail.com>
2020-04-07 14:16:50 -0700

    refactor two number sum

--- problems/individual_problems/two_number_sum.js
+++ problems/individual_problems/two_number_sum.js
@@ -4,15 +4,16 @@ two numbers in an array, else return an empty array.
 
 */
 function twoNumberSum(array, targetSum) {
-    let visited = {};
+    let visited = new Set()
+    
     for (let i = 0; i < array.length; i++) {
         let num = array[i];
         let remainder = targetSum - num;
 
-        if (!!visited[remainder]) return [remainder, num]
-        !!visited[num] ? visited[num].push(i) : visited[num] = i;
+        if (visited.has(remainder)) return [remainder, num]
+        visited.add(num)
     }
     return []
 }
 
-console.log(twoNumberSum([1, 2, 3, 4, 5, 6, 7, 8, 9], 17))
+console.log(twoNumberSum([1, 2, 3, 4, 5, 5, 6, 7, 8, 9], 17))
